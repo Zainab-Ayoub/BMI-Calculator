8c72d0e7afbd2631d1c467e3cdd6782de3aff1b5
Zainab Ayoub Zafar <151748568+Zainab-Ayoub@users.noreply.github.com>
2024-09-13 01:14:57 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-height = input("Enter you height in m: ")
-weight = input("Enter you weight in kg: ")
-BMI = int(weight)/float(height) ** 2
+height = float(input("Enter you height in m: "))
+weight = float(input("Enter you weight in kg: "))
+BMI = round(weight/height ** 2) 
+
 print(BMI)
+
+if BMI < 18.5:
+    print("You are underweight.")
+elif BMI < 25:
+    print("You have normal weight.") 
+elif BMI < 30:
+    print("You are overweight.")        
+elif BMI < 35:
+    print("You are obese.")
+else:
+    print("You are clinically obese.")            
